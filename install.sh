9b10bba6f7e5fe11d4928c9c063f1d6d02d78dfb
robbycostales <robbycostales@gmail.com>
2021-02-08 10:37:48 -0700

    Add linking functionality to install file

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
+
+# Necessary Homebrew installations
+echo "Neovim: Installing necessary packages with Homebrew"
 brew install --HEAD luajit
 brew install --HEAD neovim
 brew tap homebrew/cask-fonts
 brew install --cask font-hack-nerd-font
-brew install ctags-exuberant@5.8.2
+brew install ctags-exuberant
+
+# Create symbolic links of files
+echo "Neovim: Linking file init.vim"
+ln -sf "$PWD/init.vim" ~/.config/nvim/init.vim
+echo "Neovim: Installing plugins"
+nvim --headless +PlugInstall +qall
+
